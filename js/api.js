0f9b30a5153a76be375b599c6f72ed3813cf5205
Joao Pereira Vaz <joao.prrvz@eduge.ch>
2025-01-31 15:01:32 +0100

    Create api class JS

---
+++ js/api.js
@@ -0,0 +1,27 @@
+export class API{
+	static url = "http://localhost/jeu-esquive/backend/api.php";
+	
+	/**
+	 * @returns {Promise<{username: string, score:number}[]>} 
+	 */
+	static async selectAllUsers(){
+		let reponse = await fetch(this.url);
+		return await reponse.json();
+	}
+
+	/**
+	 * @param {string} username 
+	 * @param {number} score 
+	 * @returns {Promise<{username: string, score:number}[]>} 
+	 */
+	static async insertScore(username, score){
+		let reponse = await fetch(this.url, {
+			method: "POST",
+			body: JSON.stringify({
+				username: username,
+				score: score
+			})
+		});
+		return await reponse.json();
+	}
+}

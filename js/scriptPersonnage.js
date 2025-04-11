34068d93ccc99b43297dc6de1d12eeda73e35266
Miguel Esclr <miguel.esclr@eduge.ch>
2025-04-11 14:55:16 +0200

    implémentation d'un visuel pour le cooldown des capacités dans testPersonnage

--- js/scriptPersonnage.js
+++ js/scriptPersonnage.js
@@ -1,18 +1,61 @@
 import { AnimationStorage } from "./Storage/animationsStorage.js";
-import { Player, Fireball } from "./Entities/entities.js";
+import { Player, Fireball, Entity } from "./Entities/entities.js";
+import { PhysicsWorld, RectangleCollider as Rect, RectangleCollider, Vector } from "./PhysicsEngine/PhysicsEngine.js";
 
 
-const player = new Player(document.querySelector("#joueur"));
+const world = new PhysicsWorld(document.createElement("canvas").getContext("2d"));
+const player = new Player(document.querySelector("#joueur"), new Rect(0,0, 60, 63), {x: 70, y: 68});
+/** @type {Entity[]} */
+const entities = [player];
+world.addColliders(player.collider)
 
+/** @type {HTMLParagraphElement} */
 let score = document.querySelector("#score")
+score.style.color = "white"
 score.textContent = "0"
-let intervalScore = setInterval(()=>{
-    score.textContent = Number(score.textContent) + Number(1);
+let intervalScore = setInterval(() => {
+    score.textContent = (Number(score.textContent) + Number(1)).toString();
 }, 1000)
 score.style.position = "absolute"
-score.style.left = screen.width/2 + "px";
+score.style.left = screen.width / 2 + "px";
 score.style.top = "10px"
-//position: absolute;top:250px;left:250px;
+
+let imgFlash = document.createElement("img")
+let imgShield = document.createElement("img")
+let imgGhost = document.createElement("img")
+
+imgFlash.setAttribute("src", "img/spell/flash.jpg")
+imgFlash.style.height = "10vh"
+imgFlash.style.width = "5vw"
+
+imgShield.setAttribute("src", "img/spell/barriere.jpg")
+imgShield.style.height = "10vh"
+imgShield.style.width = "5vw"
+
+imgGhost.setAttribute("src", "img/spell/ghost.jpg")
+imgGhost.style.height = "10vh"
+imgGhost.style.width = "5vw"
+
+// Positionner les icônes de sorts en bas de l'écran
+imgFlash.style.position = "fixed";
+imgShield.style.position = "fixed";
+imgGhost.style.position = "fixed";
+
+// Définir les positions (ex : en bas à gauche, alignées horizontalement)
+imgFlash.style.bottom = "10px";
+imgFlash.style.left = "10px";
+
+imgShield.style.bottom = "10px";
+imgShield.style.left = "calc(10px + 5vw + 10px)"; // position après imgFlash
+
+imgGhost.style.bottom = "10px";
+imgGhost.style.left = "calc(10px + 5vw + 10px + 5vw + 10px)"; // position après imgShield
+
+
+let mainContainer = document.querySelector("body")
+mainContainer.appendChild(imgFlash)
+mainContainer.appendChild(imgShield)
+mainContainer.appendChild(imgGhost)
 
 let underghost = false;
 let underShield = false;
@@ -24,46 +67,58 @@ let mouseDown = false;
 addEventListener("mouseup", () => mouseDown = false)
 addEventListener("mousedown", (e) => {
     mouseDown = true
-    movePlayer(e);
+    setPlayerTarget(e);
 })
 addEventListener("contextmenu", e => e.preventDefault())
-window.addEventListener("mousemove", movePlayer);
-function movePlayer(e) {
+window.addEventListener("mousemove", setPlayerTarget);
+
+function setPlayerTarget(e) {
     if (mouseDown) {
         player.target.x = e.clientX
         player.target.y = e.clientY
-        player.vector.x = player.target.x - player.center.x;
-        player.vector.y = player.target.y - player.center.y;
-        player.normalize();
-        player.vector.x *= player.speed;
-        player.vector.y *= player.speed;
     }
 }
+const start = Date.now();
+setInterval(() => {
+    main(Date.now() - start)
+}, 1000 / 60)
 
-function move() {
-    if (!(player.target.x >= player.x && player.target.x <= player.width + player.x
-        && player.target.y >= player.y && player.target.y <= player.height + player.y)) {
-        player.x += player.vector.x;
-        player.y += player.vector.y;
-        player.vector.x = player.target.x - player.center.x;
-        player.vector.y = player.target.y - player.center.y;
-        player.normalize();
-        player.vector.x *= player.speed;
-        player.vector.y *= player.speed;
-
-    }
-    requestAnimationFrame(move);
-}
-move();
-
-Main()
 // Fonction principale qui démarre la génération des boules
-function Main() {
-    setInterval(() => {
-        GenererBoule(GenererPosition()) // Génère une boule toutes les secondes
-    }, 1000)
+//player.collider.collide.add(() => {alert("a")})
+function main(timepassed) {
+    world.update();
+    player.setVelocityToTarget();
+    player.updateAnimation();
+    for (const entity of entities) {
+        entity.draw();
+    }
+    function spawnLoop() {
+        GenererBoule(GenererPosition());
+        let interval = Math.max(300, 1000 / getDifficulty());
+        setTimeout(spawnLoop, interval);
+    }
+    if(timepassed % 500 < 10){
+        return new Promise((resolve) => {
+            if (score.textContent == (180).toString()) {
+                setTimeout(() => {
+                    spawnLoop();
+                    resolve()
+                }, 1)
+            }
+            else{
+                setTimeout(() => {
+                    spawnLoop();
+                    resolve()
+                }, 1000 - Number(score.textContent) * 100)
+            }
+        });
+    }
 }
-
+// Calcule la difficulté en fonction du score
+function getDifficulty() {
+    let currentScore = Number(score.textContent);
+    return 1 + currentScore /60 ;
+}
 // Fonction pour générer une boule à une position donnée
 function GenererBoule(p) {
     let boule = document.createElement("img") // Création d'un élément div
@@ -72,12 +127,9 @@ function GenererBoule(p) {
     body.append(boule) // Ajout de la boule au body
 
     // Définition des styles de la boule
-    boule.style.height = "50px"
-    boule.style.width = "50px"
-    boule.style.position = "absolute"
-    boule.style.top = `${p[1]}px` // Position verticale aléatoire
-    boule.style.left = `${p[0]}px` // Position horizontale aléatoire
-    let fireball = new Fireball(boule)
+    let fireball = new Fireball(boule, new Rect(p[1],p[0],40,40), {x: 117, y: 129 })
+    world.addColliders(fireball.collider);
+    entities.push(fireball);
     GenererDirection(fireball) // Démarre le mouvement de la boule
 }
 
@@ -115,117 +167,98 @@ function GenererPosition() {
     }
 }
 
-// Fonction qui génère une direction aléatoire et fait bouger la boule
+// 
+/**
+ * Fonction qui génère une direction aléatoire et fait bouger la boule
+ * @param {Fireball} p 
+ */
 function GenererDirection(p) {
-
-    p.target.x = player.x
-    p.target.y = player.y
-    p.vector.x = p.target.x - p.x;
-    p.vector.y = p.target.y - p.y;
-    p.normalize();
-    p.vector.x *= p.speed;
-    p.vector.y *= p.speed;
-    setInterval(() => {
-        movefireball(p)
-    }, 1000 / 60); // Rafraîchissement rapide pour un mouvement fluide
-}
-/** @param {Fireball} p  */
-function movefireball(p) {
-    p.x += p.vector.x;
-    p.y += p.vector.y;
-    if (p.x < 0 || p.x > screen.width) {
-        p.element.remove()
-        return
-    }
-    if (p.y < 0 || p.y > screen.height) {
-        p.element.remove()
-        return
-    }
-    if (p.x < player.x + player.width &&
-        p.x + p.width > player.x &&
-        p.y < player.y + player.height &&
-        p.y + p.height > player.y) {
-        if (underShield == false)
-        {
-            clearInterval(intervalScore)
-            localStorage.score = score.textContent;
-            window.location.href = 'gameOver.html'; 
-        }
-        p.element.remove()
-    }
+    p.target.x = player.collider.center.x;
+    p.target.y = player.collider.center.y;
+    p.setVelocityToTarget();
+    p.element.style.transform = `rotate(${trouverAngle(p, player)}deg)`;
 }
 
-let posSourisXSpell = 0;
-let posSourisYSpell = 0;
 
-document.addEventListener('mousemove', function (event) {
-    posSourisXSpell = event.clientX
-    posSourisYSpell = event.clientY
-
-});
 
 window.addEventListener("keypress", (e) => {
     console.log(e.key);
-    let posSourisX = posSourisXSpell
-    let posSourisY = posSourisYSpell
+    let posSourisX = player.target.x;
+    let posSourisY = player.target.y;
     if (e.key == "f") {
 
-        if (cdFlash == 0)
-        {
+        if (cdFlash == 0) {
+            imgFlash.setAttribute("src", "img/spell/flashUsed.jpg")
             cdFlash = 15000;
-            player.x = posSourisX + ((player.x - posSourisX) / 1.35);
-            player.y = posSourisY + ((player.y - posSourisY) / 1.35);
-            player.target.x = player.x;
-            player.target.y = player.y;
-            let timeoutCd = setTimeout((e)=>{
+            player.collider.x = posSourisX + ((player.collider.x - posSourisX) / 1.35);
+            player.collider.y = posSourisY + ((player.collider.y - posSourisY) / 1.35);
+            player.target.x = player.collider.x;
+            player.target.y = player.collider.y;
+            let timeoutCd = setTimeout((e) => {
                 cdFlash = 0;
+                imgFlash.setAttribute("src", "img/spell/flash.jpg")
             }, cdFlash)
         }
 
 
     }
     if (e.key === "e") {
-        if (underghost == false)
-        {
+        if (underghost == false) {
+            imgGhost.setAttribute("src", "img/spell/ghostUsed.jpg")
             underghost = true
             cdGhost = 15000;
             player.speed = 10;
             let count = 0;
             let isBlue = false; // Permet d'alterner entre bleu clair et bleu foncé
-    
+
             const interval = setInterval(() => {
                 player.element.style.backgroundColor = isBlue ? "blue" : "lightblue";
                 isBlue = !isBlue; // Alterne entre bleu et bleu clair
-    
+
                 count++;
                 if (count >= 30) { // 30 cycles = 15 secondes (1 cycle = 500ms)
                     clearInterval(interval);
-                    player.element.style.backgroundColor = "blue"; // Assure un retour en bleu à la fin
                     player.speed = 5; // Réinitialisation de la vitesse
-                    let timeoutCd = setTimeout((e) =>{
+                    let timeoutCd = setTimeout((e) => {
                         underghost = false
+                        imgGhost.setAttribute("src", "img/spell/ghost.jpg")
                     }, cdGhost)
                 }
             }, 500); // Alterne toutes les 500ms pour 1s complète entre bleu et bleu clair
         }
 
     }
-    if (e.key === "d")
-    {
-        if (cdShield == 0)
-        {
+    if (e.key === "d") {
+        if (cdShield == 0) {
+            imgShield.setAttribute("src", "img/spell/barriereUsed.jpg")
             underShield = true;
             cdShield = 15000
             player.element.style.border = "solid grey 3px"
-            let timeout = setTimeout((e) =>{
+            let timeout = setTimeout((e) => {
                 player.element.style.border = "none"
                 underShield = false;
+                
             }, 3000)
-            let timeoutCd = setTimeout((e) =>{
+            let timeoutCd = setTimeout((e) => {
+                imgShield.setAttribute("src", "img/spell/barriere.jpg")
                 cdShield = 0;
-            },cdShield)
+            }, cdShield)
         }
 
     }
 
 })
+
+
+/**
+ * genere un angle
+ * @param {Fireball} el1 
+ * @param {Player} el2 
+ */
+function trouverAngle(el1, el2) {
+    const deltaX = el1.collider.x - el2.collider.x
+    const deltaY = el2.collider.y - el1.collider.y
+    const angleBrute = Math.atan2(deltaX, deltaY)
+    const angle = angleBrute * (180 / Math.PI);
+    return angle
+}
